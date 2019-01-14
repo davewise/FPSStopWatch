9a5a4b8079d6e16e26f0c1365f941036e40a5529
Dave Wise <dwise@broadspotimaging.com>
2019-01-14 11:11:19 -0800

    Adds frame rate divided by 3 to interface

--- FPSStopWatch/ViewController.swift
+++ FPSStopWatch/ViewController.swift
@@ -17,13 +17,15 @@ class ViewController: UIViewController {
     @IBOutlet var titlelabel: UILabel!
     
     @IBOutlet var fpsNum: UITextField!
-    
+    @IBOutlet var fpsNumDividedBy3: UITextField!
+
     @IBOutlet var exposureTimeNum: UITextField!
     
     var timer = Timer()
     var counter = 0.00
     var isRunning = false
     var previousFPS:Double = 20
+    var previousFPSDividedBy3:Double = 6.66
     var previousExposureTime:Double = 50
     //var fps = 30.0
     //var fps = fpsNum
@@ -44,8 +46,8 @@ class ViewController: UIViewController {
     func addDoneButtonOnKeyboard() {
         let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
         doneToolbar.barStyle       = UIBarStyle.default
-        let flexSpace              = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
-        let done: UIBarButtonItem  = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.done, target: self, action: #selector(ViewController.doneButtonAction))
+        let flexSpace              = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem  = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: self, action: #selector(ViewController.doneButtonAction))
 
         var items = [UIBarButtonItem]()
         items.append(flexSpace)
@@ -55,6 +57,7 @@ class ViewController: UIViewController {
         doneToolbar.sizeToFit()
 
         self.fpsNum.inputAccessoryView = doneToolbar
+        self.fpsNumDividedBy3.inputAccessoryView = doneToolbar
         self.exposureTimeNum.inputAccessoryView = doneToolbar
     }
 
@@ -76,6 +79,16 @@ class ViewController: UIViewController {
         {
             fpsNum.text = "0"
         }
+        
+        if(!validateFPSDividedBy3())
+        {
+            fpsNumDividedBy3.text = "0"
+        }
+        if(fpsNumDividedBy3.text == "")
+        {
+            fpsNumDividedBy3.text = "0"
+        }
+        
         if(!validateExposureTime())
         {
             exposureTimeNum.text = "0"
@@ -92,20 +105,55 @@ class ViewController: UIViewController {
             if (fps != previousFPS)
             {
                 var exposureTime:Double
+                var fpsDividedBy3:Double
                 //calculate the new exposure time number
                 if(fps == 0)
                 {
                     exposureTime = 0
+                    fpsDividedBy3 = 0
                 }
                 else
                 {
                     exposureTime = round((1/fps)*1000)
+                    fpsDividedBy3 = fps/3
                 }
                 exposureTimeNum.text = String(exposureTime)
+                fpsNumDividedBy3.text = String(fpsDividedBy3)
                 previousExposureTime = exposureTime
+                previousFPSDividedBy3 = fpsDividedBy3
                 previousFPS = fps
             }
         }
+        
+        guard let fpsNumDividedBy3Text = fpsNumDividedBy3.text else {
+            return
+        }
+        if let fpsDividedBy3 = Double(fpsNumDividedBy3Text)
+        {
+            if (fpsDividedBy3 != previousFPSDividedBy3)
+            {
+                var fps:Double
+                var exposureTime:Double
+
+                if(fpsDividedBy3 == 0)
+                {
+                    fps = 0
+                    exposureTime = 0
+                }
+                else
+                {
+                    fps = fpsDividedBy3 * 3
+                    exposureTime = round((1/fps)*1000)
+                }
+                exposureTimeNum.text = String(exposureTime)
+                fpsNum.text = String(fps)
+                previousExposureTime = exposureTime
+                previousFPS = fps
+                previousFPSDividedBy3 = fpsDividedBy3
+            }
+        }
+        
+        
         guard let exposureTimeNumText = exposureTimeNum.text else {
             return
         }
@@ -114,21 +162,28 @@ class ViewController: UIViewController {
             if (exposureTime != previousExposureTime)
             {
                 var fps:Double
+                var fpsDividedBy3:Double
                 //calculate the new fps number
                 if(exposureTime == 0)
                 {
                     fps = 0
+                    fpsDividedBy3 = 0
                 }
                 else
                 {
                     fps = round(1000/exposureTime)
+                    fpsDividedBy3 = fps/3
                 }
+                
                 fpsNum.text = String(fps)
+                fpsNumDividedBy3.text = String(fps/3)
                 previousFPS = fps
                 previousExposureTime = exposureTime
+                previousFPSDividedBy3 = fpsDividedBy3
             }
         }
         self.fpsNum.resignFirstResponder()
+        self.fpsNumDividedBy3.resignFirstResponder()
         self.exposureTimeNum.resignFirstResponder()
     }
     
@@ -138,6 +193,14 @@ class ViewController: UIViewController {
         let numberFiltered = compSepByCharInSet?.joined(separator: "")
         return fpsNum.text == numberFiltered
     }
+    
+    func validateFPSDividedBy3() -> Bool {
+        let aSet = NSCharacterSet(charactersIn:"0123456789.").inverted
+        let compSepByCharInSet = fpsNumDividedBy3.text?.components(separatedBy: aSet)
+        let numberFiltered = compSepByCharInSet?.joined(separator: "")
+        return fpsNumDividedBy3.text == numberFiltered
+    }
+
     
     func validateExposureTime() -> Bool {
         let aSet = NSCharacterSet(charactersIn:"0123456789.").inverted
@@ -180,6 +243,7 @@ class ViewController: UIViewController {
             }
             if let fps = Double(text)
             {
+                //let timeInterval = (Double(1/fps)-(1/60))//1/60 to account for screen refresh
                 let timeInterval = (Double(1/fps))
                 timer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(UpdateTime), userInfo: nil, repeats: true)
                 playbutton.isEnabled = false
@@ -195,9 +259,10 @@ class ViewController: UIViewController {
         }
         if let fps = Double(text)
         {
-            let timeInterval:Double = (Double(1/fps))
+            let timeInterval:Double = (Double(1/fps)*1000)
             counter += timeInterval
-            titlelabel.text = String(format: "%.2f", counter)
+            //titlelabel.text = String(format: "%.3f", counter)
+            titlelabel.text = String(format: "%.0f", counter)
         }
     }
     
